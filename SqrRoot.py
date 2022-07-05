eaaa765fcb77de96afd9ac1b8658b42257be3029
Richard Smythe <82367391+richardsmythe@users.noreply.github.com>
2022-07-05 19:54:16 +0100

    Add files via upload

---
+++ SqrRoot.py
@@ -0,0 +1,32 @@
+
+# a = 4
+# x = 3
+# y = (x + a/x) / 2
+
+# print (y)
+
+# x = y
+# y = (x + a/x) / 2
+
+# print (y)
+
+# x = y
+# y = (x + a/x) / 2
+
+# print (y)
+
+# x = y
+# y = (x + a/x) / 2
+
+# print (y)
+
+a = 4
+x = 3
+while True:
+    print(x)
+    y = (x + a/x) / 2
+    print (y)
+    if y == x:
+        break
+    x = y
+
