993e6013d4934a0d3ce2aa56087ea63b9c4b4cdd
Richard Smythe <82367391+richardsmythe@users.noreply.github.com>
2023-10-27 20:50:54 +0100

    Update SqrRoot.py

--- SqrRoot.py
+++ SqrRoot.py
@@ -1,25 +1,3 @@
-
-# a = 4
-# x = 3
-# y = (x + a/x) / 2
-
-# print (y)
-
-# x = y
-# y = (x + a/x) / 2
-
-# print (y)
-
-# x = y
-# y = (x + a/x) / 2
-
-# print (y)
-
-# x = y
-# y = (x + a/x) / 2
-
-# print (y)
-
 a = 4
 x = 3
 while True:
